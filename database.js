473312cb1c157e1cbbc5ae9477f9ff6a1246a66f
schoolexit <kindyq7@gmail.com>
2026-06-16 11:31:27 +0700

    Dashboard: Cập nhật thông tin

--- database.js
+++ database.js
@@ -64,7 +64,7 @@ window.kindergartenData = {
             "class": "Koala 1",
             "image": "images/user.jpg",
             "sound": "",
-            "isBus": false,
+            "isBus": true,
             "busRoute": ""
         }
     ]
