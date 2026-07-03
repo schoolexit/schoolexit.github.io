4b7de3d010538fc6c5ecee73a330d2f77b7ed4d2
schoolexit <kindyq7@gmail.com>
2026-07-03 09:10:52 +0700

    Dashboard: Cập nhật

--- database.js
+++ database.js
@@ -126,8 +126,8 @@ window.kindergartenData = {
         },
         {
             "id": "st-1781580563385",
-            "name": "Hà Duy AN",
-            "nickname": "Duy Duy",
+            "name": "Nguyễn Nhựt Anh",
+            "nickname": "Clouis",
             "branch": "TanQuy Campus",
             "class": "Koala 1",
             "image": "images/img_1781580557563.jpg",
