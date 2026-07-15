6887fcf672418d596ca1848a6aa6dc82f84862c7
schoolexit <kindyq7@gmail.com>
2026-07-15 13:59:03 +0700

    Dashboard: Cập nhật

--- database.js
+++ database.js
@@ -1,6 +1,30 @@
 window.kindergartenData = {
     "classes": [
         {
+            "id": "cl-1781591664919",
+            "name": "Koala",
+            "branch": "HimLam Campus",
+            "image": "images/icon/koala.png"
+        },
+        {
+            "id": "cl-17828882069170.18700691993405727",
+            "name": "Panda",
+            "branch": "HimLam Campus",
+            "image": "images/icon/panda.png"
+        },
+        {
+            "id": "cl-17828882069170.6064161715832719",
+            "name": "Dolphin",
+            "branch": "HimLam Campus",
+            "image": "images/icon/dolphin.png"
+        },
+        {
+            "id": "cl-17828882069180.0948946666514725",
+            "name": "Giraffe",
+            "branch": "HimLam Campus",
+            "image": "images/icon/giraffe.png"
+        },
+        {
             "id": "cl-1784081169091",
             "name": "Koala",
             "branch": "VDT Campus",
